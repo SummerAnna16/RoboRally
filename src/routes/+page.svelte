1b2551830a914f6cecd25d3748c8b9ac1e76191d
Anna Nicolaou <annanicolaou@Annas-MacBook-Air.local>
2022-09-11 18:57:31 -0400

    Learning how {#each} lets us loop over an array

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+	const pi = Math.PI;
+    const animals_i_love = [ "aardvarks", "dogs", "cats", "geckos", "snakes", "rabbits" ];
+</script>
+
+<h1>Hello, this is the first header</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<p>The value of pi is {pi}</p>
+
+<p>Here's a list of animals I love:</p>
+<ul>
+{#each animals_i_love.sort((a, b) => a.length - b.length) as animal}
+<li>{animal}</li>
+{/each}
+</ul>
